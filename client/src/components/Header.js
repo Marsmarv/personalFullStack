71671e312791052fa77da16dc3b6f0f912eb6d2d
Marvin Romero <marvin.r02@gmail.com>
2019-10-06 13:06:09 -0400

    git module

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ()=>{
+  return(
+    <div className="header">
+      The <span id="met-span"><a id="met-link" href="https://www.metmuseum.org/" target="_blank">Met</a></span>ropolitan Museum of Art
+    </div>
+  )
+}
+
+export default Header
